d75b3e718152033b643f2506c93c7640fe986fc6
Joseph E. Curtis <madscatt@gmail.com>
2017-06-15 12:33:46 -0400

    working pdbscan

--- trunk/sassie/build/gui_mimic_pdbscan.py
+++ trunk/sassie/build/gui_mimic_pdbscan.py
@@ -4,6 +4,7 @@ import logging
 sys.path.append('./')
 
 import sassie.build.pdb_scan as pdb_scan
+#import sassie.build.pdbscan as pdbscan
 
 import sassie.interface.input_filter as input_filter
 import multiprocessing
@@ -16,9 +17,9 @@ svariables = {}
 
 runname='run_0'
 pdbfile='testing/data/3MWY.pdb'
-pdbfile='testing/data/5E3L.pdb'
-pdbfile='testing/data/1D0O.pdb'
-pdbfile='hiv1_gag_ma.pdb'
+#pdbfile='testing/data/5E3L.pdb'
+#pdbfile='testing/data/1D0O.pdb'
+#pdbfile='hiv1_gag_ma.pdb'
 
 #### end user input ####
 #### end user input ####
@@ -37,6 +38,7 @@ if(len(error)>0):
 txtQueue=multiprocessing.JoinableQueue()   
 
 scan = pdb_scan.PDBScan()
+#scan = pdbscan.pdbscan(variables,txtQueue)
 scan.main(variables,txtQueue)
 
 this_text = txtQueue.get(True, timeout=0.1)
